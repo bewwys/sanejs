3a92edbccb07c514bf49b550aa4707513ecc40d0
me <me@me.com>
2022-11-11 13:10:34 +0100

    Success at recursively_make_dom() for basic render component widget. (ui_section and do_button)

--- maybehappy/main.js
+++ maybehappy/main.js
@@ -1,17 +1,19 @@
 // Log: Start 09:07
 
 global_ui_context = {
-    current_el              : null,
-    root                    : null,
-    layout                  : null,
-    vdom                    : {},
-    virtual_append          : null,
-    previous_virtual_append : null,
-    previous_vdom           : {tag_name    : "MAIN",
-                               attrs       : {id:"root"},
-                               children    : [],
-                               parent_node : {}},
-    where_ami               : {}
+    current_el: null,
+    root: null,
+    layout: null,
+    vdom: {},
+    virtual_append: null,
+    previous_virtual_append: null,
+    previous_vdom: {
+        tag_name: "MAIN",
+        attrs: { id: "root" },
+        children: [],
+        parent_node: {}
+    },
+    where_ami: {}
 }
 
 global_ui_context.previous_vdom.parent_node = global_ui_context.previous_vdom
@@ -20,49 +22,46 @@ global_ui_context.previous_vdom.parent_node = global_ui_context.previous_vdom
 // el : Element
 // @-@
 function ui_begin(el) {
-    global_ui_context.layout                  = el;
-    global_ui_context.vdom.tag_name           = el.tagName;
-    global_ui_context.vdom.attrs              = {id: el.getAttribute("id")};
-    global_ui_context.vdom.children           = [];
-    global_ui_context.vdom.parent_node         = global_ui_context.vdom;
-    global_ui_context.virtual_append          = global_ui_context.vdom.children;
+    global_ui_context.layout = el;
+    global_ui_context.vdom.tag_name = el.tagName;
+    global_ui_context.vdom.attrs = { id: el.getAttribute("id") };
+    global_ui_context.vdom.children = [];
+    global_ui_context.vdom.parent_node = global_ui_context.vdom;
+    global_ui_context.virtual_append = global_ui_context.vdom.children;
     global_ui_context.previous_virtual_append = global_ui_context.virtual_append;
     // TODO(): We need to refactor to create a beautiful and comprehensive
     //         object.
-    global_ui_context.where_ami               = global_ui_context.vdom;
+    global_ui_context.where_ami = global_ui_context.vdom;
 }
 
 // @-@
-// vel : Virtual_Element 
+// vel : Virtual_Element
+// rel : Real_Element 
 // @-@
 
-// I'm so tired right now but I have an idea maybe take the parentNode 
-// when we want to append. Recursively append the children to the papa.
-// We need some kind of the root where all the recursive will be made.
 // TODO(): Bug; We append the papa node with itself causing a problem. 
 // TODO(): bug; We try to get the parent node with getElementById but we didn't
 //         render it on the dom yet so parent_node = null.
-function recursively_make_real_node(vel) {
-    console.log("//// vel start ////");
-    console.log(vel);
-    console.log("//// vel end ////");
+// TODO(): Make sure to test this thouroughly.
+// NOTE(): Looks like we can make recursively make a dom from a starting point element.
+//         Need thourough testing though.
+function recursively_make_real_node(vel, rel) {
+    // NOTE(): We are assuming that the first vel exist in the current dom.
     // TODO(): FOR NOW WE ASSUME THAT WE ALWAYS PASS A VEL!!!!!
-    let node = document.createElement(vel.tag_name);
-    let papa_vel = vel.parent_node;
-    let papa_node = document.getElementById(papa_vel.attrs.id);
-    node.setAttribute("id", vel.attrs.id);
-    node.setAttribute("innerHTML", vel.attrs.innerHTML);
-    result_append = node;
+
+    let el = document.getElementById(vel.attrs.id);
+    if (el === null){
+        let node = document.createElement(vel.tag_name);
+        node.setAttribute("id", vel.attrs.id);
+        node.innerHTML = vel.attrs.innerHTML;
+        rel.appendChild(node)
+        if (vel.children.length > 0) {
+            rel = node;
+        }
+    }
     for (let i = 0; i < vel.children.length; i++) {
-        result_append = recursively_make_real_node(vel.children[i]);
+        recursively_make_real_node(vel.children[i], rel);
     }
-    if (papa_node != result_append) {
-        papa_node.appendChild(result_append);
-    }
-    console.log("//// result_append start ////");
-    console.log(node);
-    console.log("//// result_append end ////");
-    return papa_node;
 }
 
 // @-@
@@ -70,8 +69,8 @@ function recursively_make_real_node(vel) {
 // previous_vdom_vel : Virtual_Element
 // @-@
 function go_deep(current_vdom_vel, previous_vdom_vel) {
-     // Check with previous vdom if they match go deep.
-     // NOTE(): Keep in mind that current_vdom_vel is a children thus an array.
+    // Check with previous vdom if they match go deep.
+    // NOTE(): Keep in mind that current_vdom_vel is a children thus an array.
     if (current_vdom_vel.children.length === previous_vdom_vel.children.length) {
         for (let i = 0; i < current_vdom_vel.children.length; i++) {
             if (current_vdom_vel.children[i].length === 0) {
@@ -88,15 +87,16 @@ function go_deep(current_vdom_vel, previous_vdom_vel) {
         // console.log("////////////////////////////:");
         // NOTE(): Check if sub operation or add operation to the previous dom.
         if (previous_vdom_vel.children.length === 0) {
-            // console.log("Add operation");
+            console.log("Add operation");
             // console.log(current_vdom_vel);
             let papa = document.getElementById(current_vdom_vel.attrs.id);
             // Create real node from current_vdom
-            let from_current_vdom_to_real_node = recursively_make_real_node(current_vdom_vel);
+            let rel = document.getElementById(current_vdom_vel.attrs.id);
+            let from_current_vdom_to_real_node = recursively_make_real_node(current_vdom_vel, rel);
             // Append new node to current real dom
             // Make sure vdoms matches.
 
-            
+
         }
         if (current_vdom_vel.children.length === 0) {
             // console.log("Sub operation");
@@ -122,11 +122,12 @@ function ui_end(el) {
 // @-@
 function do_button(id, text, p_node) {
     // global_ui_context.layout.appendChild(el);
-        global_ui_context.virtual_append.push({
-            tag_name    : "BUTTON",
-            attrs       : {id: id, innerHTML: text},
-            children    : [],
-            parent_node : global_ui_context.where_ami});
+    global_ui_context.virtual_append.push({
+        tag_name: "BUTTON",
+        attrs: { id: id, innerHTML: text },
+        children: [],
+        parent_node: global_ui_context.where_ami
+    });
 }
 
 // @-@
@@ -136,10 +137,10 @@ function do_button(id, text, p_node) {
 function ui_begin_section(id_sec, p_node) {
     // global_ui_context.layout.appendChild(el);
     global_ui_context.virtual_append.push({
-        tag_name : "SECTION",
-        attrs    : {id: id_sec, innerHTML: null},
-        children : [],
-        parent_node : p_node
+        tag_name: "SECTION",
+        attrs: { id: id_sec, innerHTML: null },
+        children: [],
+        parent_node: p_node
     });
     global_ui_context.where_ami = global_ui_context.virtual_append[global_ui_context.virtual_append.length - 1];
     // global_ui_context.layout = el;
@@ -165,11 +166,19 @@ function render() {
     ui_begin(root);
         do_button("btn-1", "mon bouton", global_ui_context.where_ami);
         ui_begin_section("sec-1", global_ui_context.where_ami);
-            do_button("btn-2", "mon deuxième bouton", global_ui_context.where_ami);
+            do_button("btn-2", "btn-2", global_ui_context.where_ami);
+            do_button("btn-3", "btn-3", global_ui_context.where_ami);
+            do_button("btn-4", "btn-4", global_ui_context.where_ami);
+        ui_end_section(root, global_ui_context.previous_virtual_append);
+        ui_begin_section("sec-2", global_ui_context.where_ami);
+            do_button("btn-5", "btn-5", global_ui_context.where_ami);
+            do_button("btn-6", "btn-6", global_ui_context.where_ami);
+            do_button("btn-7", "btn-7", global_ui_context.where_ami);
         ui_end_section(root, global_ui_context.previous_virtual_append);
     ui_end(root);
 }
 
 render();
 
+// TODO(): For now this failed badly
 // setInterval(render, 16)
